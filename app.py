548a60552e4d5390b4d7dfc04f14ad00f8bcfd4f
Wendy <wendy@pop-os.home>
2023-12-15 17:03:03 +0100

    add comment

--- app.py
+++ app.py
@@ -14,18 +14,24 @@ pages = ["Context du projet", "Exploration de données", "Visualisation de doné
 
 data = pd.read_csv("./laptops.csv", encoding='ISO-8859-1')
 data_cleaned = pd.read_csv("./laptops_cleaned.csv").drop(columns=['Unnamed: 0'],axis=1)
+
 st.sidebar.title("Sommaire")
 
 page = st.sidebar.radio("", pages)
 
 if page == pages[0]: 
+    
     st.image("./laptop.png")
     st.write("## Context du projet")
     st.write("Ce projet s'inscrit dans un contexte de ventes d'ordinateur. L'objectif est de prédire le prix des ordinateurs et de découvrir le prix de l'ordinateur a partir de ses features. ")
-    
+    st.write('Pour cela, nous avons accès à un fichier “laptops.csv” comportant des données sur les différents composant d’un ordinateur.')
 
 
 elif page == pages[1]:
+    
+    # The code snippet you provided is creating a user interface using Streamlit to display the
+    # exploration and visualization of data.
+    
     st.write("## Exploration de données")
     st.write('')
     st.dataframe(data.head())
@@ -84,11 +90,14 @@ elif page == pages[2]:
     
     
 elif page == pages[3]:
+    # The code snippet you provided is creating a user interface using Streamlit to allow users to
+    # input the features of a laptop and make a price prediction using a trained machine learning
+    # model.
     model = pickle.load(open("./knr_model.pkl", "rb"))
     
     option = st.selectbox('Choisir la marque',data_cleaned['Manufacturer'].unique())
     
-    screen_Size = st.number_input('Screen Size')
+    screen_Size = st.selectbox('Screen Size',np.sort(data_cleaned['Screen Size'].unique() ))
     ram = st.selectbox('RAM',np.sort(data_cleaned['RAM'].unique() ))
     storage = st.number_input('Storage')
     weight = st.number_input('Weight')
@@ -96,11 +105,26 @@ elif page == pages[3]:
     hdd = st.checkbox('HDD')
     
     row = np.array([screen_Size,ram,storage,weight,ssd,hdd] )
-    def predict():
-        
-        
     
-        
+    
+    def predict():
+        """
+        This function takes in user input for the features of a laptop and uses the trained model to predict the price of the laptop.
+
+        Parameters:
+        -----------
+        screen_size (int): The screen size of the laptop.
+        ram (str): The RAM of the laptop.
+        storage (int): The storage capacity of the laptop.
+        weight (int): The weight of the laptop.
+        ssd (bool): A boolean indicating whether the laptop has an SSD or not.
+        hdd (bool): A boolean indicating whether the laptop has an HDD or not.
+
+        Returns:
+        --------
+        float: The predicted price of the laptop.
+
+        """
         st.write(model.predict(row.reshape(1, -1))) 
         
         
